4f15139828818e22755456ffa03504c3e7b29684
AnaseVe <87339737+AnaseVe@users.noreply.github.com>
2022-03-29 15:58:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function lipstickPage() {
+    window.location = "lipstick.html" ;
+}
+function whiskerPage()
+{
+    window.location = "KittyWiskers.html" ;
+}
+function earringPage()
+{
+    window.location = "Earrings.html" ;   
+}
